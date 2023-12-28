1dd92b31fd951dc86236cecb53a4eadc650115a8
Jordan Allen <land.of.jordan@gmail.com>
2023-12-28 16:21:19 -0600

    additional component setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+
+export const AppRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'blog-list', component: BlogListComponent },
+    { path: 'blog-post/:id', component: BlogPostComponent },
+    { path: 'create-post', component: CreatePostComponent }
+];
 
 export const routes: Routes = [];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AppRoutes } from './app/app.routes';
+import { ROUTES } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: ROUTES, useValue: AppRoutes, multi: true },
+    // ... other providers if needed ...
+  ],
+}).catch(err => console.error(err));
